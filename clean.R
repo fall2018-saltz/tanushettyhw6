92de6b7dab232c88c5ae455fcff623a34674f4d2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:43:12 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,5 +1,5 @@
 
-clean_data <- raw_data
+
 func1 <- function(dfStates1)
   {
   dfStates1 <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors = FALSE)
@@ -26,3 +26,4 @@ func1 <- function(dfStates1)
   
   return(dfStates1)
 }
+
