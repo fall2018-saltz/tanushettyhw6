63a118a917fb7600b296e7b998f541ff2b3bf2ea
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:52:55 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,10 +2,7 @@
 
 func1 <- function(dfStates1)
   {
-  dfStates1 <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"), stringsAsFactors = FALSE)
-  
-  
-  
+  dfStates1 <- readcsv(raw_data)
   #Clean the dataframe
   #Removing the last Row(Puerto Rico). Using the '-' sign.
   dfStates1<- dfStates1[-53,]
