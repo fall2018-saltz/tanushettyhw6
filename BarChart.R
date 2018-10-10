57af287d25f1695a6c408b86ca8d44d0bd5fc6bc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 18:19:30 -0400

    Saving BarChart.R

--- BarChart.R
+++ BarChart.R
@@ -1,1 +1,26 @@
 
+#importing library for ggplot2 to generate barcharts
+library("ggplot2")
+#calculating the number of murders per state and then displaying the new subset of mergeDataframe
+#in murdersPerState
+mergeDF$numMurders <- as.integer(mergeDF$population*mergeDF$Murder/100000)
+murdersPerState <- mergeDF[,c(2,3,4,10)]
+murdersPerState
+
+#generating a barchart for number of murders per state using ggplot() and aes() to 
+#visualize the graph
+barChart1 <- ggplot(mergeDF, aes(x=stateName, y=numMurders)) + geom_col() + ggtitle("Bar chart of murders per state")
+barChart1
+
+#generating a barchart to rotate text on x-axis using theme() and aes() to visualize the graph
+barChart2 <- ggplot(mergeDF, aes(x=stateName, y=numMurders)) + geom_col() + ggtitle("Total Murders") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+barChart2
+
+#generating a barchart of sorted murder rate using ggplot() and aes() to visualize the graph
+barChart3 <- ggplot(mergeDF, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1)) + geom_col() + ggtitle("Sorted bar chart of murders per state")
+barChart3
+
+#generating a barchart to show percentOver18 as color of the bar using ggplot() and 
+#aes() to visualize the graph
+barChart4 <- ggplot(mergeDF, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1)) + geom_col(aes(size=percentOver18,color=percentOver18)) + ggtitle("Sorted bar chart showing percentOver18 as color of bar")
+barChart4
