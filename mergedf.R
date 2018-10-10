b7753037093b2ffb327c14fff86a051d016fb9db
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:34:29 -0400

    Saving mergedf.R

--- mergedf.R
+++ mergedf.R
@@ -2,8 +2,9 @@
 #Copying USArrest into a local variable
 arrests <- USArrests
 
+#Creating a column name for rows having states using rownames()
 rownames(func1) <- as.vector(func1[,"stateName"])
 
-
+#merging 2 dataframes with attributes from both dataframes
 mergeDF <- merge(func1,arrests, by="row.names")
 mergeDF
