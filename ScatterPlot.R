8e1e14ba63b70b1697eb3ee4daf3326c8cd81cd8
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 21:40:33 -0400

    Saving ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -2,5 +2,5 @@
 library("ggplot2")
 #Generating a scatter plot having population on the X axis, the percent over 18 on the y axis, 
 #and the size & color representing the murder rate
-scatterplot <- ggplot(mergeDataframe, aes(x= population, y =percentOver18)) + geom_point(aes(size = Murder, color = Murder)) + ggtitle("Scatter Plot")
-scatterplot
+ScatterPlot <- ggplot(mergeDF, aes(x= population, y =percentOver18)) + geom_point(aes(size = Murder, color = Murder)) + ggtitle("Scatter Plot")
+ScatterPlot
