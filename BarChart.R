0f32d877fc6d7d19c3889a84c616f1e66509d61e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 23:04:25 -0400

    Saving BarChart.R

--- BarChart.R
+++ BarChart.R
@@ -11,15 +11,14 @@ murdersPerState
 barChart1 <- ggplot(mergeDF, aes(x=stateName, y=numMurders)) + geom_col() + ggtitle("Bar chart of murders per state")
 barChart1
 
-#Creating a barchart for number of murders per state using ggplot()
+#Creating a barchart to rotate text on x-axis using theme() and aes() to visualize the graph
 barChart2 <- ggplot(mergeDF, aes(x=stateName, y=numMurders)) + geom_col() + ggtitle("Total Murders") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 barChart2
 
-#generating a barchart of sorted murder rate using ggplot() and aes() to visualize the graph
+#Creating a barchart for number  of murders per state using ggplot()
 barChart3 <- ggplot(mergeDF, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1)) + geom_col() + ggtitle("Sorted bar chart of murders per state")
 barChart3
 
-#generating a barchart to show percentOver18 as color of the bar using ggplot() and 
-#aes() to visualize the graph
+#Creating a barchart to show percentOver18 as color of the bar using ggplot()
 barChart4 <- ggplot(mergeDF, aes(x = reorder(stateName, numMurders), y = numMurders, group = 1)) + geom_col(aes(size=percentOver18,color=percentOver18)) + ggtitle("Sorted bar chart showing percentOver18 as color of bar")
 barChart4
