91664912ce5feaf718fae3d0448603b72f82c380
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:06:06 -0400

    Saving generating_histogram.R

--- generating_histogram.R
+++ generating_histogram.R
@@ -1,9 +1,8 @@
 
 #importing library for ggplot2
 library("ggplot2")
-#using ggplot() to generate a histogram with population on the x-axis from the mergeDataframe
-#dataset. We are using geom_histogram() function with binwidth attribute to group the data 
-#into bins. Here, we are using 500,000 as binwidth because the population is in millions.
+#Creating a histogram for population using ggplot()
+#Using geom_histogram() with binwidth attribute to group the data 
 histpop <- ggplot(mergeDF, aes(x=population)) + geom_histogram(binwidth=500000) + ggtitle("Histogram of Population")
 histpop
 
